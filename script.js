d6ed47c69da7d667f2972ad99135ede7199e17ea
AlissonGuimaraes98 <alissonguimaraes478@gmail.com>
2024-07-25 10:46:04 -0300

    3D Effect Hover

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('hosting').addEventListener('click', function() {
+    window.open('', '_blank');
+});
+
+document.getElementById('web-design').addEventListener('click', function() {
+    window.open('', '_blank');
+});
+
+document.getElementById('backend-development').addEventListener('click', function() {
+    window.open('', '_blank');
+});
+
+document.getElementById('seo').addEventListener('click', function() {
+    window.open('', '_blank');
+});
